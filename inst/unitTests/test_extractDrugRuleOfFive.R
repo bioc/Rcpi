ca8f5eb3d5a7617414e9eeecf847c36c62ba2d2b
Nan Xiao <road2stat@gmail.com>
2017-06-21 15:53:34 +0000

    GitHub release v1.13.4
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_5/madman/Rpacks/Rcpi@130594 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_extractDrugRuleOfFive.R
+++ inst/unitTests/test_extractDrugRuleOfFive.R
@@ -1,4 +1,4 @@
-test_extractDrugALOGP = function() {
+test_extractDrugRuleOfFive = function() {
     smi = c(
         'c1ccc2c(c1)oc(=O)c(C(CC(=O)C)c1ccc([N+](=O)[O-])cc1)c2O',
         'c1(ccc(cc1)O)NC(=O)C',
@@ -7,6 +7,6 @@ test_extractDrugALOGP = function() {
         'O=S(=O)(c1ccc(cc1)C(=O)C)NC(=O)NC1CCCCC1',
         'CC(=O)NO')
     mol = readMolFromSmi(textConnection(smi), type = 'mol')
-    dat = extractDrugALOGP(mol)
-    checkEqualsNumeric(sum(as.matrix(dat)), 315.1292, tolerance = 1e+0)
+    dat = extractDrugRuleOfFive(mol)
+    checkEqualsNumeric(sum(as.matrix(dat)), 0, tolerance = 1e+0)
 }
